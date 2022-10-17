10044f0a2edfdc467868bf537c6aeaef5cc4c3b1
vominhtung <vmtung5000@gmail.com>
2022-10-17 13:47:50 +0700

    java

---
+++ CircleComparatorTest.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+//import java.util.Comparator;
+
+
+
+public class CircleComparatorTest {
+    public static void main(String[] args) {
+        ComparableCircle[] circles = new ComparableCircle[3];
+        circles[0] = new ComparableCircle(3.6);
+        circles[1] = new ComparableCircle();
+        circles[2] = new ComparableCircle(3.5, "indigo", false);
+
+        System.out.println("Pre-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+
+//       Comparator circleComparator = new CircleComparator();
+//        Arrays.sort(circles,circleComparator);
+           Arrays.sort(circles);
+        System.out.println("After-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+    }
+}
